c2485ce375861105a8e46fc3e2e391912438ec44
sahanabela <sahanabela@gmail.com>
2019-11-11 18:50:17 -0600

    Change UI theme and made the app flow correctly

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,139 +1,182 @@
+// import 'package:flutter/material.dart';
+// import 'package:hashpost/screens/login_screen.dart';
+// import 'package:hashpost/screens/screens.dart';
+// import 'package:hashpost/screens/settings_screen.dart';
+// import 'package:hashpost/shared/shared.dart';
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home", style: Theme.of(context).textTheme.title),        
+//         elevation: 0.0,
+//         actions: <Widget>[
+//           Padding(
+//              padding: const EdgeInsets.all(0.5),
+//             child: IconButton(
+//               icon: Icon(Icons.settings),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (BuildContext context) => SettingsScreen()),
+//                 );
+//               }
+//             ),
+            
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(0.5),
+//             child: IconButton(
+//               icon: Icon(Icons.perm_identity),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (BuildContext context) => LoginScreen()),
+//                 );
+//               }
+//             ),
+//           ),
+//         ],
+//       ),
+//       backgroundColor: Theme.of(context).primaryColor,
+//       body: Column(
+//         children: <Widget>[
+//           Container(
+//             padding: EdgeInsets.symmetric(horizontal: 12.0),
+//             height: MediaQuery.of(context).size.height/2.5,
+//             width: MediaQuery.of(context).size.width,
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: <Widget>[
+//                 Padding(
+//                   padding: EdgeInsets.only(top: 20.0, bottom:20.0, left: 10.0, right: 2.0),
+//                   child: Container(
+//                     color: Color(0xffE3E4E6),
+//                     child: new ImageButton(),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(top: 20.0, bottom:20.0, left: 2.0, right: 10.0),
+//                   child: Container(
+//                     color: Color(0xffE3E4E6),
+//                     child: new CameraButton(),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Container(
+//             margin: const EdgeInsets.all(7.0),
+//             height: MediaQuery.of(context).size.height/2.5,
+//             width: MediaQuery.of(context).size.width,
+//             child: GridView.count(
+//               crossAxisCount: 3,
+//               //crossAxisSpacing: 10.0,
+//               children: <Widget>[
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://data.whicdn.com/images/327767201/superthumb.jpg?t=1552284504"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://i.ytimg.com/vi/oW_xiyNJg2w/hqdefault.jpg"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://i.imgur.com/t1LuvGZ.jpg"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://i.redd.it/qghnuxkd7r821.jpg"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://66.media.tumblr.com/1de8078095f6f18409bc0b32bba7e52a/tumblr_pmgxfnMPLk1ufzrtb_540.jpg"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://i.redd.it/4tjd030s3sg11.png"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://picsum.photos/200"),
+//                 ),
+//                 PostTile(
+//                   side: MediaQuery.of(context).size.width/4,
+//                   child: Image.network("https://picsum.photos/200"),
+//                 ),
+//               ],
+//             )
+//           )
+//         ],
+//       )
+//     );
+//   }
+// }
+
+
+// class PostTile extends StatelessWidget {
+//   final double side;
+//   final Widget child;
+//   const PostTile({Key key, this.side = 20, this.child = const Icon(Icons.photo)}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: const EdgeInsets.all(7.0),
+//       color: Color(0xffE3E4E6),
+//       height: side,
+//       width: side,
+//       child: GridTile(
+//         child: child,
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:hashpost/screens/login_screen.dart';
-import 'package:hashpost/screens/screens.dart';
-import 'package:hashpost/screens/settings_screen.dart';
-import 'package:hashpost/shared/shared.dart';
+import 'package:flutter/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key key}) : super(key: key);
+import 'login_screen.dart';
 
+class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     backgroundColor: Colors.black,   
       appBar: AppBar(
-        title: Text("Home", style: Theme.of(context).textTheme.title),        
-        elevation: 0.0,
+        elevation: 0,
+        backgroundColor: Colors.black,
         actions: <Widget>[
-          Padding(
-             padding: const EdgeInsets.all(0.5),
-            child: IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (BuildContext context) => SettingsScreen()),
-                );
-              }
-            ),
-            
-          ),
-          Padding(
-            padding: const EdgeInsets.all(0.5),
-            child: IconButton(
-              icon: Icon(Icons.perm_identity),
+            // action button
+            IconButton(
+              icon: Icon(Icons.arrow_forward, color: Colors.white),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (BuildContext context) => LoginScreen()),
                 );
-              }
-            ),
-          ),
-        ],
+              },
+            ),]
       ),
-      backgroundColor: Theme.of(context).primaryColor,
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            height: MediaQuery.of(context).size.height/2.5,
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom:20.0, left: 10.0, right: 2.0),
-                  child: Container(
-                    color: Color(0xffE3E4E6),
-                    child: new ImageButton(),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom:20.0, left: 2.0, right: 10.0),
-                  child: Container(
-                    color: Color(0xffE3E4E6),
-                    child: new CameraButton(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(7.0),
-            height: MediaQuery.of(context).size.height/2.5,
-            width: MediaQuery.of(context).size.width,
-            child: GridView.count(
-              crossAxisCount: 3,
-              //crossAxisSpacing: 10.0,
-              children: <Widget>[
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://data.whicdn.com/images/327767201/superthumb.jpg?t=1552284504"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://i.ytimg.com/vi/oW_xiyNJg2w/hqdefault.jpg"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://i.imgur.com/t1LuvGZ.jpg"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://i.redd.it/qghnuxkd7r821.jpg"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://66.media.tumblr.com/1de8078095f6f18409bc0b32bba7e52a/tumblr_pmgxfnMPLk1ufzrtb_540.jpg"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://i.redd.it/4tjd030s3sg11.png"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://picsum.photos/200"),
-                ),
-                PostTile(
-                  side: MediaQuery.of(context).size.width/4,
-                  child: Image.network("https://picsum.photos/200"),
-                ),
-              ],
-            )
+      body: Container(
+        alignment: Alignment.center,
+       //padding: EdgeInsets.all(2.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/logo 2.png"),
+            Text("hashpost.",  style: TextStyle(color: Colors.white, fontSize: 30.0, )),
+            Text("socials made easier",  style: TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.w100)),
+          ],
           )
-        ],
+         
+
       )
     );
   }
 }
-
-
-class PostTile extends StatelessWidget {
-  final double side;
-  final Widget child;
-  const PostTile({Key key, this.side = 20, this.child = const Icon(Icons.photo)}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(7.0),
-      color: Color(0xffE3E4E6),
-      height: side,
-      width: side,
-      child: GridTile(
-        child: child,
-      ),
-    );
-  }
-}
-
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'createaccount_screen.dart';
+import 'editing_screen.dart';
 
 
 // class LoginScreen extends StatelessWidget {
@@ -136,31 +137,43 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Login"),
+        backgroundColor: Colors.black,
+        elevation: 0.0,
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Container(
           alignment: Alignment.center,
-          padding: EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(50.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Welcome to"),
-              Text("HashPost"),
+              Image.asset("assets/images/logo 2.png"),
+              Text("hashpost.", style: TextStyle(fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.w500)),
+              Text("socials made easier.", style: TextStyle(fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.w500)),
               LoginWidget(),
-              Row(
-                children: [
-                  Text("Don't have an account?"),
-                  GestureDetector(
-                    child: Text("Create one here!"),
-                    onTap: () {
-                      MaterialPageRoute(builder: (context) => CreateAccountScreen());
-                    }
-                  )
-                ]
+              
+              Container(
+                alignment: Alignment.center,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?", style: TextStyle(fontSize: 11.0, color: Colors.grey)),
+                    GestureDetector(
+                      child: Text(" Let's make one!", style:TextStyle(color: Colors.redAccent, fontSize: 11.0), textAlign: TextAlign.left),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (BuildContext context) => CreateAccountScreen()),
+                        );
+                      }
+                    )
+                  ]
+                ),
               )
             ]
           )
@@ -187,12 +200,32 @@ class _LoginWidgetState extends State<LoginWidget> {
     return Container(
       child: Column(
         children: [
+          Padding(padding: EdgeInsets.all(15.0),),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(5.0),
+            child: FlatButton(
+              child: Text("Continue with Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+              color: Colors.blue[600],
+               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              onPressed: () {
+                print("pressed");
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text("OR LOG IN WITH EMAIL", style: TextStyle(fontSize: 11.0, color: Colors.white, fontWeight: FontWeight.w100)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
             child: TextField(
               controller: _email,
               decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[850],
                 hintText: "Email",
+                hintStyle: TextStyle(color: Colors.grey[700]),
               )
             ),
           ),
@@ -201,18 +234,30 @@ class _LoginWidgetState extends State<LoginWidget> {
             child: TextField(
               controller: _password,
               decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[850],
+                hintStyle: TextStyle(color: Colors.grey[700]),
                 hintText: "Password",
               ),
               obscureText: true
             ),
           ),
+          Padding(padding: EdgeInsets.all(10.0),),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: FlatButton(
-              child: Text("Login"),
+              
+              child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+              color: Colors.green,
+               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
               onPressed: () {
                 print(_email.text);
                 print(_password.text);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (BuildContext context) => EditingScreen()),
+                );
               },
             ),
           )
